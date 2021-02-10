f246048f409bfc57d6e9a26eea3ead94e202416d
Andrey-Kashin <kolbasyk@list.ru>
2021-02-10 13:48:21 +0300

    add section-slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
 	$(".slider__inner").slick({
 		dots: true,
+		speed: 800,
 		prevArrow: '<button class="section-slider__btn section-slider__btnprev"><img src = "/img/slider-1/prew-arrow.svg" alt="previous"></button>',
 		nextArrow: '<button class="section-slider__btn section-slider__btnnext"><img src = "/img/slider-1/next-arrow.svg" alt="next"></button>',
 	});
