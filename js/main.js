5bfb60dfa641e343da96cb076ed9e0965f235a50
Andrey-Kashin <kolbasyk@list.ru>
2021-02-14 02:19:00 +0300

    add product-card

--- js/main.js
+++ js/main.js
@@ -6,12 +6,19 @@ $(function () {
 		nextArrow: '<button class="section-slider__btn section-slider__btnnext"><img src = "/img/slider-1/next-arrow.svg" alt="next"></button>',
 	});
 
-	$('.search__link').on('click', function(e){
+
+	$('.tab').on('click', function(e){
 		e.preventDefault();
-		$('.search__link').removeClass('search__link--active');
-		$('.search__form-inner').removeClass('search__form-inner--active');
-		$(this).addClass('search__link--active');
-		$($(this).attr('href')).addClass('search__form-inner--active');
+		$('.tab').removeClass('tab--active');
+		$('.tabs__content').removeClass('tabs__content--active');
+		$(this).addClass('tab--active');
+		$($(this).attr('href')).addClass('tabs__content--active');
+	});
+
+	// Замена цвета на кнопке при клике мышью
+
+	$('.tabs-container__button-favorite').on('click', function(){
+		$($(this)).toggleClass('button-favorite--active');
 	});
 
 });
