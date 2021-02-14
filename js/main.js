cb02c185fe03a06fe3c31f61d06c8fd438ce04b7
Andrey-Kashin <kolbasyk@list.ru>
2021-02-14 17:07:09 +0300

    add section products

--- js/main.js
+++ js/main.js
@@ -2,23 +2,35 @@ $(function () {
 	$(".slider__inner").slick({
 		dots: true,
 		speed: 800,
-		prevArrow: '<button class="section-slider__btn section-slider__btnprev"><img src = "/img/slider-1/prew-arrow.svg" alt="previous"></button>',
-		nextArrow: '<button class="section-slider__btn section-slider__btnnext"><img src = "/img/slider-1/next-arrow.svg" alt="next"></button>',
+		prevArrow: '<button class="section-slider__btn section-slider__btnprev"><img src = "../img/slider-1/prew-arrow.svg" alt="previous"></button>',
+		nextArrow: '<button class="section-slider__btn section-slider__btnnext"><img src = "../img/slider-1/next-arrow.svg" alt="next"></button>',
 	});
 
 
-	$('.tab').on('click', function(e){
+	$('.tab').on('click', function (e) {
 		e.preventDefault();
-		$('.tab').removeClass('tab--active');
-		$('.tabs__content').removeClass('tabs__content--active');
+		$($(this).siblings()).removeClass('tab--active');
+		$($(this).parent().siblings().find('div')).removeClass('tabs__content--active');
 		$(this).addClass('tab--active');
 		$($(this).attr('href')).addClass('tabs__content--active');
+
+		// Принудительная инициализация слайдера
+
+		$('.product-slider').slick('setPosition');
 	});
 
 	// Замена цвета на кнопке при клике мышью
 
-	$('.tabs-container__button-favorite').on('click', function(){
+	$('.tabs-container__button-favorite').on('click', function () {
 		$($(this)).toggleClass('button-favorite--active');
+	});
+
+
+	$(".product-slider").slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<button class="product-slider__btn product-slider__btnprev"><img src = "../img/products/product-slider-arrow-prew.svg"></button>',
+		nextArrow: '<button class="product-slider__btn product-slider__btnnext"><img src = "../img/products/product-slider-arrow-next.svg" alt="next"></button>',
 	});
 
 });
