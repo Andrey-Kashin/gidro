b1abd08e381ae4f47ce4413f3d7b722db30e24ba
Andrey-Kashin <kolbasyk@list.ru>
2021-03-02 22:53:35 +0300

    adaptive slider1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function () {
 		speed: 800,
 		prevArrow: '<button class="section-slider__btn section-slider__btnprev"><img src = "../img/slider-1/prew-arrow.svg" alt="previous"></button>',
 		nextArrow: '<button class="section-slider__btn section-slider__btnnext"><img src = "../img/slider-1/next-arrow.svg" alt="next"></button>',
+		responsive: [
+			{
+				breakpoint: 770,
+				settings: {
+					arrows: false,
+				}
+			}
+		]
+
 	});
 
 
@@ -74,5 +83,9 @@ $(function () {
 		});
 
 	});
+
+	$('.mobile-menu__btn').click(function() {
+    $('.mobile-menu').toggleClass('mobile-menu-active');
+});
 
 });
